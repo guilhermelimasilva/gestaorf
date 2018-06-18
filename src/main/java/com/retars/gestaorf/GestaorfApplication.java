198e1296572c73230ed5da8442b526dc217b3b3e
Guilherme Lima <lima-guilherme@hotmail.com>
2018-06-18 20:31:00 -0300

    Criação do Projeto

---
+++ src/main/java/com/retars/gestaorf/GestaorfApplication.java
@@ -0,0 +1,12 @@
+package com.retars.gestaorf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaorfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaorfApplication.class, args);
+	}
+}
